58368343a29f876c956612c31caad3fad04362ee
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2019-12-31 16:28:20 +0530

    instead of sqlite3 , now we use mongoDB for storage

--- quotetut/quotetut/pipelines.py
+++ quotetut/quotetut/pipelines.py
@@ -5,38 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sqlite3
-
+import pymongo
 
 class QuotetutPipeline(object):
 
     def __init__(self):
-        self.create_connection()
-        self.create_table()
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        db = self.conn['quotes']
+        self.collection = db['quotes_tb']
 
-    def create_connection(self):
-        self.conn = sqlite3.connect("myquotes.db")
-        self.curr = self.conn.cursor()
 
-    def create_table(self):
-        self.conn.execute("""DROP TABLE IF EXISTS quoted_tb""")
-        self.conn.execute("""create table quoted_tb(
-                        title text,
-                        author text,
-                        tag text
-        )""")
 
 
     def process_item(self, item, spider):
-        self.store_db(item)
-#        print("Pipeline:" + item['title'][0])
+        self.collection.insert(dict(item))
         return item
 
 
-    def store_db(self,item):
-        self.curr.execute("""insert into quoted_tb values (?,?,?)""",(
-            item['title'][0],
-            item['author'][0],
-            item['tag'][0]
-        ))
-        self.conn.commit()
