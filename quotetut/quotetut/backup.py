ec6d49afaca8c230bff69b3e95f8591c921ab1fd
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2019-12-31 18:00:19 +0530

    logging in a site using forms and crawling multiple pages by changin page number in url

--- quotetut/quotetut/backup.py
+++ quotetut/quotetut/backup.py
@@ -1,30 +1,20 @@
 import scrapy
-from scrapy.http import FormRequest
 from ..items import QuotetutItem
-from scrapy.utils.response import open_in_browser
+
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
-
+    page_number = 2
     start_urls = [
-        'http://quotes.toscrape.com/login'
+        'http://quotes.toscrape.com/page/1/'
     ]
 
     def parse(self, response):
-
-        token = response.css('form input::attr(value)').extract_first()
-        return FormRequest.from_response(response, formdata={
-
-            'csrf_token': token,
-            'username': 'girish@gmail.com',
-            'password': 'hello123'
-        },callback = self.start_scraping)
-
-    def start_scraping(self,response):
-        open_in_browser(response)
         items = QuotetutItem()
         all_div_quotes = response.css('div.quote')
 
+
         for quotes in all_div_quotes:
+
             title = quotes.css('span.text::text').extract()
             author = quotes.css('.author::text').extract()
             tag = quotes.css('.tag::text').extract()
@@ -34,3 +24,11 @@ class QuoteSpider(scrapy.Spider):
             items['tag'] = tag
 
             yield items
+
+        next_page = 'http://quotes.toscrape.com/page/'+str(QuoteSpider.page_number)+'/'
+        if QuoteSpider.page_number <11 :
+            QuoteSpider.page_number += 1
+            yield response.follow(next_page, callback = self.parse)
+
+
+
