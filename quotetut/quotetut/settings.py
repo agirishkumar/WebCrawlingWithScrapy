c715303b25f612aa2d22421432157549bf41fe06
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2019-12-31 17:00:25 +0530

    crawling multiple pages by changin page number in url

--- quotetut/quotetut/settings.py
+++ quotetut/quotetut/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    'quotetut.pipelines.QuotetutPipeline': 300,
-}
+#ITEM_PIPELINES = {
+#    'quotetut.pipelines.QuotetutPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
